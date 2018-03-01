e9f2c5183ae46694514dd0e1d03235065512f933
Salman <salmankhurshid.me@gmail.com>
2018-03-02 05:18:12 +0530

    Created Webform Project

---
+++ Assignment/App_Code/DemoRectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Demo Class for testing purpose
+/// </summary>
+public class DemoRectangle
+{
+  private Point P;
+  private int Width;
+  private int Length;
+
+  public DemoRectangle(int x, int y, int width, int length)
+  {
+    this.P = new Point(x, y);
+    this.Width = width;
+    this.Length = length;
+  }
+
+  public void DrawRectangle(Graphics myGraphics, Pen myPen)
+  {
+
+    Point p1 = this.P;
+    Point p2 = new Point(p1.X + this.Width, p1.Y);
+    Point p3 = new Point(p1.X + this.Width, p1.Y + this.Length);
+    Point p4 = new Point(p1.X, p1.Y + this.Length);
+
+    myGraphics.DrawLine(myPen, p1, p2);
+    myGraphics.DrawLine(myPen, p2, p3);
+    myGraphics.DrawLine(myPen, p3, p4);
+    myGraphics.DrawLine(myPen, p4, p1);
+  }
+}
---
+++ Assignment/App_Code/Intersection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Intersection
+/// </summary>
+public class Intersection 
+{
+  public bool IsIntersecting(MyRectangle rec1, MyRectangle rec2)
+  {
+    if (rec1.XValue + rec1.WidthValue > rec2.XValue || rec2.XValue + rec2.WidthValue > rec1.XValue ||
+        rec1.YValue + rec1.HeightValue > rec2.YValue || rec2.YValue + rec2.HeightValue > rec1.YValue)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
